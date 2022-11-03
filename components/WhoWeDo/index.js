70cc23dcd91ebcfb3c3a5fad646b058adfb647d2
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-11-03 11:43:10 -0300

    Who We Do section

---
+++ components/WhoWeDo/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import data from './data.json';
+import { WhoWeDoCard, WhoWeDoCards, WhoWeDoContainer } from './style';
+
+function WhoWeDo() {
+    
+  return (
+    <WhoWeDoContainer>
+        <div className='who__we__do__content'>
+            <h2>Como <br/>fazemos</h2>
+            <WhoWeDoCards>
+                {data.cards.map ((card, index) => {
+                    return (
+                            <WhoWeDoCard key={index}>
+                                <h3>{card.title}</h3>
+                                <p>{card.desc}</p>
+                            </WhoWeDoCard>
+                        )
+                    }
+                )}
+                <WhoWeDoCard>
+                    
+                </WhoWeDoCard>
+            </WhoWeDoCards>
+        </div>
+
+    </WhoWeDoContainer>
+  )
+}
+
+export default WhoWeDo
---
+++ components/WhoWeDo/style.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const WhoWeDoContainer = styled.section`
+    background-color: ${({theme}) => theme.primary};
+    padding: 9rem 1rem 5.6rem;
+    margin-top: -60px;
+
+    .who__we__do__content {
+        max-width: 900px;
+        width: 100%;
+        margin: 0 auto;
+
+        h2 {
+            font-family: 'Saira', sans-serif;
+            font-size: 3rem;
+            line-height: 45px;
+            color: ${({theme}) => theme.white};
+            margin-bottom: 3.2rem;
+        }
+    }
+`
+
+export const WhoWeDoCards = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 2rem;
+`
+
+export const WhoWeDoCard = styled.div`
+    background-color: ${({theme}) => theme.white};
+    border-radius: 20px;
+    color: ${({theme}) => theme.primary};
+    max-width: 270px;
+    width: 100%;
+    min-height: 290px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+
+    h3 {
+        font-size: 2rem;
+        font-family: 'Saira', sans-serif;
+        line-height: 30px;
+        margin-bottom: 1rem;
+    }
+    
+    p {
+        font-family: 'Saira', sans-serif;
+
+    }
+
+    &:last-child {
+        background-color: ${({theme}) => theme.secondary};
+        border-radius: 20px 20px 100% 20px;
+    }
+`
