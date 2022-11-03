2fabfa4613dab1bfa0335eafaa7f999c0ae6c653
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-11-03 11:52:17 -0300

    Changes for deploy

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Header from '../Components/Header'
-import BannerHero from '../components/BannerHero'
-import WhoWeDo from '../components/WhoWeDo'
-import Careers from '../components/Careers'
-import CloseToYou from '../components/CloseToYou'
-import Depoiments from '../components/Depoiments'
-import Form from '../components/Form'
-import Footer from '../components/Footer'
+import Head from 'next/head';
+import Header from '../components/Header';
+import BannerHero from '../components/BannerHero';
+import WhoWeDo from '../components/WhoWeDo';
+import Careers from '../components/Careers';
+import CloseToYou from '../components/CloseToYou';
+import Depoiments from '../components/Depoiments';
+import Form from '../components/Form';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
