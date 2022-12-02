087621a1cdafbadba34e01dea40766c6d58ec556
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-12-02 09:01:35 -0300

    Adding facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,50 @@ import { Toaster } from 'react-hot-toast';
 import GlobalStyles from '../styles/globals';
 import NextNProgress from 'nextjs-progressbar';
 import theme from '../styles/theme';
+import Router from 'next/router';
+import Script from 'next/script';
+import React from 'react';
+
+function FacebookPixel() {
+  React.useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('557013655243129');
+        ReactPixel.pageView();
+
+        Router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  });
+  return null;
+}
 
 function MyApp({ Component, pageProps }) {
+
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <>
+      <Script
+        strategy='lazyOnload'
+        id='GA-scriptOne'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        strategy='lazyOnload'
+        id='GA-scriptTwo'
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+      <FacebookPixel />
       <ThemeProvider theme={theme}>
         <Toaster position="bottom-right" />
         <NextNProgress
